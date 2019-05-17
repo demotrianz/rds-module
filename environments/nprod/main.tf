7b43a263ec8022c66c9d1117c8bb59f42f51c220
demotrianz <49957165+demotrianz@users.noreply.github.com>
2019-05-17 15:05:08 +0530

    Update main.tf

--- environments/nprod/main.tf
+++ environments/nprod/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-        bucket = "terra001bucket"
+        bucket = "nprod001"
         key = "terraform.tfstate"
         region = "us-east-1"
         // profile = "nprod"
