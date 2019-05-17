8c0ebe8f71f70461c24f74423a63be23a11dcea2
demotrianz <49957165+demotrianz@users.noreply.github.com>
2019-05-17 13:42:08 +0530

    Update main.tf

--- environments/nprod/main.tf
+++ environments/nprod/main.tf
@@ -9,9 +9,9 @@ terraform {
         bucket = "terra001bucket"
         key = "terraform.tfstate"
         region = "us-east-1"
-        profile = "nprod"
-        // access_key = ""
-       // secret_key = ""
+        // profile = "nprod"
+        access_key = "AKIAZGBKAQECPDWRAY7S"
+        secret_key = "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"
     }
 }
 
